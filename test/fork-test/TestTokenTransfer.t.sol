8aaf9d41d374621d587554a76f3ace9f1febf420
Riiz0 <shawnrizo@hotmail.com>
2024-10-02 18:55:19 -0400

    Edit: via_ir support 0.8.26 require custom error

---
+++ test/fork-test/TestTokenTransfer.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { Test, console2 } from "forge-std/Test.sol";
+import { TokenTransfer } from "../../src/TokenTransfer/TokenTransfer.sol";
+import { HelperConfig } from "../../script/HelperConfig.s.sol";
+
+contract TestTokenTransfer is Test {
+    function setUp() public { }
+
+    function test_one() public { }
+
+    function test_two() public { }
+}
